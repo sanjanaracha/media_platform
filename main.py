5f449463f71695354ee9d2390ce40b2ef6aef0fc
sanjanaracha <sanjanaracha16@gmail.com>
2026-05-20 20:01:29 +0530

    dashboard created with upload files feature with session_state

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import streamlit as st
+from d_base import conn_obj,cursor_obj
+
 st.title("Media Platform")
 
-login,signup=st.tabs(
-    ["Login","Sign up"]
-    )
+if "users" not in st.session_state:
+    st.session_state=None
 
-with signup:
+
+def dashboard():
+    st.sidebar.success("welcome user!!!")
+    opt=st.sidebar.selectbox("choose:--",["UploadFiles","ViewFile","Logout"])
+    st.header("Dashboard")
+
+    if opt=="UploadFiles":
+        st.header("upload your file here")
+        choosedfile=st.file_uploader("choose:--",type=["pdf","jpg","jpeg","mp3","mp4","png"])
+
+        if choosedfile:
+            st.write(choosedfile.name)
+            st.write(choosedfile.type)
+        
+    
+
+
+
+def signup_fun():
     st.header("SignUp")
     with st.form("login_form"):
         name=st.text_input("name")
@@ -13,9 +32,37 @@ with signup:
         password=st.text_input("password",type="password")
         btn=st.form_submit_button("signup")
 
-with login:
+        if btn:
+            query="insert into users (name,email,password) values (%s,%s,%s)"
+            values=(name,email,password)
+            cursor_obj.execute(query,values)
+            conn_obj.commit()
+            st.write("user added successfully")
+
+def login_fun():
     st.header("Login")
     with st.form("login"):
         email=st.text_input("email")
         password=st.text_input("password",type="password")
         btn=st.form_submit_button("Login")
+
+        if btn:
+            query="select * from users where email=%s and password=%s"
+            values=(email,password)
+            cursor_obj.execute(query,values)
+            loggedin_user=cursor_obj.fetchone()
+            st.session_state.user=loggedin_user
+            st.write("loggedin successfully")
+            st.rerun()
+
+
+if st.session_state.user==None:
+    login,signup=st.tabs(
+        ["Login","SignUp"]
+    )
+    with signup:
+        signup_fun()
+    with login:
+        login_fun()
+else:
+    dashboard()
